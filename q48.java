b0ca07714b546db859bfb0954759060474ea576b
Magul-L <doublelight77@gmail.com>
2024-11-10 20:32:58 -0500

    commit

--- q48.java
+++ q48.java
@@ -1,7 +1,17 @@
 public class q48 {
     class Solution {
         public void rotate(int[][] matrix) {
-            
+            int n = matrix.length;
+            int[][] tmp = new int[n][];
+            for(int i=0;i<n;i++){
+                tmp[i] = matrix[i].clone();
+            }
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    matrix[j][n - 1 - i] = tmp[i][j];
+                }
+            }
+
         }
     }
 }
